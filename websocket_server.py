85c55d077c0d3695dd883fb886047a96f585fdfc
Florian PAQUET <contact@florianpaquet.com>
2014-02-03 21:36:29 +0100

    Added logging config

--- websocket_server.py
+++ websocket_server.py
@@ -1,5 +1,40 @@
+import logging.config
 from mease import Mease
 from mease.backends.redis import RedisBackend
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            '()': 'colorlog.ColoredFormatter',
+            'format': '[%(asctime)s] [%(levelname)s] %(log_color)s%(message)s',
+            'log_colors': {
+                'DEBUG': 'bold_blue',
+                'INFO': 'bold_yellow',
+                'WARNING': 'bold_orange',
+                'ERROR': 'bold_red',
+                'CRITICAL': 'bold_red'
+            }
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'mease': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': 0
+        },
+    },
+}
+
+logging.config.dictConfig(LOGGING)
 
 mease = Mease(RedisBackend, {})
 
